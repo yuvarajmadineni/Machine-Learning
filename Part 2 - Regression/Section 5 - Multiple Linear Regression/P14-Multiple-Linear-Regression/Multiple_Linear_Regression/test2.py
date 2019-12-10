a07962469ee13b8c3e877b60782081f69f57f08d
raghavendraivv <ivvraghavendra1999@gmail.com>
2019-12-10 22:10:51 +0530

    Multi  linear Regression

--- Part 2 - Regression/Section 5 - Multiple Linear Regression/P14-Multiple-Linear-Regression/Multiple_Linear_Regression/test2.py
+++ Part 2 - Regression/Section 5 - Multiple Linear Regression/P14-Multiple-Linear-Regression/Multiple_Linear_Regression/test2.py
@@ -13,7 +13,8 @@ x[:, 3] = le_x.fit_transform(x[:, 3])
 o = OneHotEncoder(categorical_features = [3])
 x = o.fit_transform(x).toarray()
 
-
+"""avoinding duming variables trap"""
+x=x[:, 1:]
 
 
 from sklearn.cross_validation import train_test_split
@@ -25,3 +26,13 @@ reg = LinearRegression()
 reg.fit(x_train, y_train)
 
 y_pred = reg.predict(x_test)
+
+import statsmodels.formula.api as sm
+x = np.append(arr = np.ones([50,1]).astype(int), values = x,axis = 1 )
+x_opt = x[:, [0,1,2,3,4,5]]
+reg_OLS = sm.OLS(endog = y, exog = x_opt).fit()
+print(reg_OLS.summary())
+x_opt = x[:, [0,3]]
+reg_OLS = sm.OLS(endog = y, exog = x_opt).fit()
+print(reg_OLS.summary())
+
